812e124e02996ea0a9866d7ccba1ba2b6a86e11d
Maxim <mixam07@ukr.net>
2022-09-29 15:56:00 +0300

    add first window auth

--- src/components/Auth/Auth.jsx
+++ src/components/Auth/Auth.jsx
@@ -1,5 +1,7 @@
 import s from "./Auth.module.css";
 import Header from "../HeaderAuth/HeaderAuth";
+import cookie from "../../assets/icons/cookie.svg";
+import cupcake from "../../assets/icons/cupcake.svg";
 
 const Auth = (props) => {
     return(
@@ -7,10 +9,37 @@ const Auth = (props) => {
             <section className={s.auth}>
                 <Header title="Реєстрація" />
                 <div className={s.main}>
-                    main
+                    <div className={s.item}>
+                        <div className={s.wrapper}>
+                            <div className="img img_75">
+                                <img src={cookie} alt="cookie" />
+                            </div>
+                            <div className={s.info}>
+                                <h2 className={s.title}>Покупець</h2>
+                                <h3 className={s.subtitle}>Хочу купувати, дарувати та їсти</h3>
+                            </div>
+                        </div>
+                        <div className={s.button}>
+                            <div className={s.btn}>&rsaquo;</div>
+                        </div>
+                    </div>
+                    <div className={s.item}>
+                        <div className={s.wrapper}>
+                            <div className="img img_75">
+                                <img src={cupcake} alt="cookie" />
+                            </div>
+                            <div className={s.info}>
+                                <h2 className={s.title}>Кондитер</h2>
+                                <h3 className={s.subtitle}>Хочу випікати, прикрашати та продавати</h3>
+                            </div>
+                        </div>
+                        <div className={s.button}>
+                            <div className={s.btn}>&rsaquo;</div>
+                        </div>
+                    </div>
                 </div>
                 <div className={s.footer}>
-                    footer
+                    <button className={s.href}>Я вже маю акаунт, хочу просто увійти</button>
                 </div>
             </section>
         </div>
